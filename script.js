84e373203671a7540c2f6958526394a961a923b3
lamine <laminejuvee@gmail.com>
2021-11-27 22:08:14 +0100

    added all operations

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ let operator = "";
 let num1 = "";
 let num2 = "";
 let numOperator = 0;
+let result = "";
 
 let numbers = document.querySelectorAll('.number');
 numbers.forEach(numb => {
@@ -49,7 +50,57 @@ operations.forEach(opr => {
 let btnEqual = document.querySelector('#btnEqual');
 btnEqual.addEventListener('click', getResult);
 
+let btnPoint = document.querySelector('#btnPoint');
+btnPoint.addEventListener('click', addPoint);
+
+let btnClear = document.querySelector('#btnClr');
+btnClear.addEventListener('click',clear);
+
+let btnDel = document.querySelector('#btnDel');
+btnDel.addEventListener('click',delet);
+
+let btnPercent = document.querySelector('#btnPercent');
+btnPercent.addEventListener('click',percent);
+
+function percent(e){
+  let currentVal = document.getElementById("main").value;
+  if (currentVal!==""){
+    result = +currentVal/100;
+    document.getElementById("main").value = result;
+  } else {
+    document.getElementById("main").value = "Enter a number first";
+    result = "Error";
+  }
+}
+
+function delet(e){
+ let currentVal = document.getElementById("main").value;
+ document.getElementById("main").value = currentVal.slice(0, -1);
+ }
+
+function clear(e){
+ operator = "";
+ num1 = "";
+ num2 = "";
+ numOperator = 0;
+ result = "";
+ document.getElementById("main").value = "";
+ document.getElementById("sec").value = "";
+}
+
+function addPoint (e){
+  const value = e.target.innerHTML;
+  let currentVal = document.getElementById("main").value;
+  if (!currentVal.includes(".")){
+    document.getElementById("main").value += value;
+  } 
+}
+
 function display(e) {
+  if (result !== ""){
+    document.getElementById("main").value = "";
+    result = "";
+  }
   if (operator === "") {
     const value = e.target.innerHTML;
     document.getElementById("main").value += value;
@@ -62,8 +113,9 @@ function display(e) {
 }
 function selectOpr(e) {
   if (num2 !== "") {
-    let result = operate(num1, operator, num2);
-    document.getElementById("sec").value = result;
+    result = operate(num1, operator, num2);
+    let roundedResult = Number(Math.round(result+'e2')+'e-2');
+    document.getElementById("sec").value = roundedResult;
     document.getElementById("main").value = "";
     console.log(operator);
     num1 = result;
@@ -77,12 +129,18 @@ function selectOpr(e) {
   }
 }
 function getResult() {
-  let result = operate(num1, operator, num2);
-  let sec = document.getElementById("main").value;
-  document.getElementById("sec").value += sec;
-  document.getElementById("main").value = result;
-  console.log(operator);
-  num1 = result;
-  num2 = "";
-  operator = "";
+  if (num1!=="" && operator!=="" && num2!==""){
+    result = operate(num1, operator, num2);
+    let sec = document.getElementById("main").value;
+    document.getElementById("sec").value += sec;
+    let roundedResult = Number(Math.round(result+'e2')+'e-2');
+    document.getElementById("main").value = roundedResult;
+    console.log(operator);
+    num1 = result;
+    num2 = "";
+    operator = "";
+  } else {
+    document.getElementById("main").value = "not a valid operation";
+    result = "Error";
+  } 
 }
